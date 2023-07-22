1ad0afe149a18fbbc217b59e803ea7fb9e131c44
LeoLins10 <140223550+LeoLins10@users.noreply.github.com>
2023-07-21 23:09:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,50 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+# Definir a tabela de correspondência
+correspondencia = {
+    # ... (código omitido para brevidade)
+}
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Definir as perguntas e as opções de resposta
+perguntas = [
+    # ... (código omitido para brevidade)
+]
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+opcoes = [
+    # ... (código omitido para brevidade)
+]
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Função para calcular os resultados com base nas respostas
+def calcular_resultados(respostas):
+    resultados = {}
+    for atributo in correspondencia:
+        resultados[atributo] = 0
+        for i, resposta in enumerate(respostas):
+            resultados[atributo] += correspondencia[atributo][int(resposta == "a")]
+    return resultados
 
+# Aplicativo web interativo com Streamlit
+def main():
+    st.title("Questionário de Personalidade")
+    
+    # Criar um dicionário para armazenar as respostas
+    respostas = {}
+    
+    # Perguntar ao usuário todas as questões
+    for i, pergunta in enumerate(perguntas):
+        st.write(f"**Pergunta {i+1}:** {pergunta}")
+        opcao_selecionada = st.radio("", opcoes[i])
+        respostas[i] = "a" if opcao_selecionada == opcoes[i][0] else "b"
+    
+    # Calcular os resultados com base nas respostas
+    resultados = calcular_resultados(list(respostas.values()))
+    
+    # Exibir os resultados em uma tabela interativa
+    df_resultados = pd.DataFrame(list(resultados.items()), columns=["Atributo", "Pontuação"])
+    st.write("**Resultados:**")
+    st.dataframe(df_resultados)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Executar o aplicativo
+if __name__ == "__main__":
+    main()
