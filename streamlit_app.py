782cbae56cba71d010277374e340c7a660321f77
LeoLins10 <140223550+LeoLins10@users.noreply.github.com>
2023-07-21 23:18:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,51 @@ import streamlit as st
 
 # Definir a tabela de correspondência
 correspondencia = {
-    # ... (código omitido para brevidade)
+   "Lógico": [2, 0],
+    "Intuitivo": [1, 0],
+    "Dedicado": [0, 2],
+    "Flexível": [0, 1],
+    "Líder": [0, 2],
+    "Colaborativo": [1, 0],
+    "Criativo": [2, 1],
+    "Aventureiro": [1, 0],
+    "Planejador": [2, 0],
+    "Espontâneo": [1, 0],
+    "Comunicativo": [2, 0],
+    "Empático": [1, 1],
+    "Analítico": [2, 0],
+    "Assertivo": [0, 2],
+    "Resiliente": [2, 0],
+    "Tranquilo": [0, 1],
+    "Ambicioso": [2, 0],
+    "Equilibrado": [0, 1]
 }
 
 # Definir as perguntas e as opções de resposta
 perguntas = [
-    # ... (código omitido para brevidade)
+    "Quando se trata de tomar decisões, você é mais:",
+    "Como você lida com desafios e adversidades?",
+    "Quando se trata de trabalhar em equipe, você é mais:",
+    "Quais são suas principais fontes de inspiração?",
+    "Como você lida com mudanças e incertezas?",
+    "Quando se trata de se comunicar com outras pessoas, você é mais:",
+    "Quais são suas principais habilidades?",
+    "Como você se relaciona com outras pessoas em seu ambiente pessoal e profissional?",
+    "Quando se trata de lidar com o estresse, você é mais:",
+    "Quais são suas principais motivações e objetivos na vida?"
 ]
 
 opcoes = [
-    # ... (código omitido para brevidade)
+     ["Lógico e racional", "Intuitivo e emocional"],
+    ["Com determinação e perseverança", "Com flexibilidade e adaptabilidade"],
+    ["Líder e assertivo", "Colaborativo e cooperativo"],
+    ["Arte e cultura", "Natureza e aventura"],
+    ["Com resiliência e inovação", "Com segurança e estabilidade"],
+    ["Comunicativo e expressivo", "Empático e compreensivo"],
+    ["Criatividade e imaginação", "Análise e lógica"],
+    ["Sociável e extrovertido", "Reservado e introvertido"],
+    ["Resiliente e determinado", "Relaxado e tranquilo"],
+    ["Realização pessoal e profissional", "Conexão emocional e bem-estar"]
 ]
 
 # Função para calcular os resultados com base nas respostas
