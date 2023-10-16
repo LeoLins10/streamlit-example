21d2e93e6463dd88ae970632dd1229958a8fd186
LeoLins10 <140223550+LeoLins10@users.noreply.github.com>
2023-10-16 01:50:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,55 @@
 import streamlit as st
 
-# Dicionário de mapeamento de letras para respostas
-mapeamento_letras_respostas = {
-    'A': "Inocente",
-    'B': "Explorador",
-    'C': "Sábio",
-    'D': "Herói",
-    'E': "Fora da Lei",
-    'F': "Mago",
-    'G': "Cara Comum",
-    'H': "Amante",
-    'I': "Comediante",
-    'J': "Prestativo",
-    'K': "Criador",
-    'L': "Governante"
+# Dicionário de grupos e suas características
+grupos = {
+    "Grupo A - Idealista": ["Abrimos mão do material para entender nossos valores", "Ainda acreditamos que é possível viver no paraíso", "Buscamos a paz e tranquilidade agora"],
+    "Grupo B - Caminhante": ["Não pertencemos a ninguém nem a lugar algum", "Gostamos de explorar experiências", "Estamos sempre em movimento"],
+    "Grupo C - Sapiente": ["Buscamos experiências que nos fazem crescer", "Acreditamos que a felicidade é resultado da educação", "Buscamos aprendizado e sabedoria"],
+    "Grupo D - Valente": ["Somos competitivos e competentes", "Queremos fazer do mundo um lugar melhor", "Somos corajosos e cheios de energia"],
+    "Grupo E - Rebelde": ["Desobediência é a nossa estratégia para a mudança", "Violamos leis e normas em benefício do bem-estar alheio", "Quebramos regras por aventura"],
+    "Grupo F - Alquimista": ["Transformamos sonhos em realidade", "Gostamos de rituais, momentos simbólicos", "Trazemos magia e encantamento para a vida"],
+    "Grupo G - Habitante": ["Exaltamos as virtudes de ser comum", "Acreditamos que coisas e momentos bons é um direito de todos", "Acreditamos que todos são importantes, exatamente como são"],
+    "Grupo H - Entusiasta": ["Buscamos a satisfação dos desejos pessoais", "Valorizamos todos os tipos de amor", "Desenvolvemos habilidades emocionais"],
+    "Grupo I - Comediante": ["Somos a vida da festa", "Sabemos que é possível ser você mesmo e aceito pelos outros", "Somos brincalhões e espontâneos"],
+    "Grupo J - Altruísta": ["Somos altruístas, movidos por compaixão e generosidade", "Nos sacrificamos pelos outros", "Estamos mais preocupados com o bem-estar alheio do que com o nosso"],
+    "Grupo K - Artista": ["Autenticidade e curiosidade são a base da alma", "Temos espírito empreendedor, inconformado", "Gostamos de nos expressar em formas materiais"],
+    "Grupo L - Regente": ["Temos como objetivo tornar a vida estável e previsível", "Temos tudo sob controle", "Assumimos a liderança, somos responsáveis"]
 }
 
-def main():
-    st.title("Questionário de Múltipla Escolha")
+# Dicionário de polos
+polos = {
+    "Polo Singularidade": ["Grupo D - Valente", "Grupo F - Alquimista", "Grupo K - Artista"],
+    "Polo Liberdade": ["Grupo B - Caminhante", "Grupo E - Rebelde", "Grupo I - Comediante"],
+    "Polo Coletividade": ["Grupo G - Habitante", "Grupo H - Entusiasta", "Grupo J - Altruísta"],
+    "Polo Regularidade": ["Grupo A - Idealista", "Grupo C - Sapiente", "Grupo L - Regente"]
+}
 
-    afirmacoes = [
-        "Somos satisfeitos pela beleza e as maravilhas da vida (A)",
-        "Abrimos mão do material para entender nossos valores (A)",
-        "Buscamos a paz e tranquilidade agora (A)",
-        # Adicione todas as afirmações aqui
-    ]
+# Função para fazer uma pergunta e obter a resposta do usuário
+def fazer_pergunta(pergunta):
+    while True:
+        resposta = input(pergunta + " (Sim/Não): ").strip().lower()
+        if resposta in ["sim", "não"]:
+            return resposta
+        else:
+            print("Por favor, responda com 'Sim' ou 'Não'.")
 
-    respostas = []
+# Função para calcular o polo com base nas respostas
+def calcular_polo(respostas):
+    contador_polos = {"Polo Singularidade": 0, "Polo Liberdade": 0, "Polo Coletividade": 0, "Polo Regularidade": 0}
+    for grupo, perguntas in grupos.items():
+        for pergunta in perguntas:
+            resposta = fazer_pergunta(f"Você concorda com a afirmação: {pergunta}")
+            if resposta == "sim":
+                for polo, grupos_polo in polos.items():
+                    if grupo in grupos_polo:
+                        contador_polos[polo] += 1
 
-    for afirmacao in afirmacoes:
-        checkbox = st.checkbox(afirmacao)
-        if checkbox:
-            letra = afirmacao[-2]
-            respostas.append(mapeamento_letras_respostas.get(letra, ""))
+    polo_final = max(contador_polos, key=contador_polos.get)
+    return polo_final
 
-    if st.button("Calcular Resposta"):
-        resultado = ', '.join(respostas)
-        st.write(f"Respostas: {resultado}")
+# Perguntas e respostas
+polo_resultante = calcular_polo(polos)
 
-if __name__ == "__main__":
-    main()
+# Exibir o resultado final
+print("\nVocê se encaixa no", polo_resultante)
+
